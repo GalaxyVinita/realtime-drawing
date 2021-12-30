803a8cf5a1a85de877ba7972a0b6069f2d8d19c2
GalaxyVinita <75704799+GalaxyVinita@users.noreply.github.com>
2021-12-29 19:28:44 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
+noseX = 0;
+noseY = 0;
+difference = 0;
+leftWristX = 0;
+rightWristX = 0;
 
 function setup(){
     video = createCapture(VIDEO);
@@ -16,12 +21,26 @@ function modelLoaded(){
 
 function draw(){
     background('#969A97');
+    document.getElementById("square_side").innerHTML = "Width and Height of a square will be =" + difference + "px";
+    fill("maroon");
+    stroke("maroon");
+    square(noseX, noseY, difference);
 }
 
 function gotPoses(results){
     if(results.length > 0)
     {
         console.log(results);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        console.log("noseX = " + noseX + "noseY = " + noseY);
+    
+        leftWristX = results[0].pose.leftWrist.x;
+        rightWristX = results[0].pose.rightWrist.x;
+        difference = floor(leftWristX - rightWristX);
+   
+        console.log("leftWristX = " + leftWristX + "rightWristX = " + rightWristX + "difference = " + difference);
+
     }
 }
 
